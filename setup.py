4e99d001d9060992ebc3de6cc0e0ac1910b91fa8
Rahul Sridhar <sridhar.rahul@gmail.com>
2018-10-01 21:26:10 -0700

    Restructured it to become a real python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name             = 'cryptonite',
+  version          = '0.1.0',
+  description      = 'Python Swiss Army Knife for CTF Cryptography challenges',
+  url              = 'https://github.com/fortenforge/cryptonite',
+  author           = 'fortenforge',
+  author_email     = 'i.am@fortenf.org',
+  packages         = ['cryptonite']
+)
